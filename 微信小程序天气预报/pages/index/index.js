f6661b0a5f541b4bb577fbd9861748be0be834cc
moi <m13169613348@163.com>
2017-11-15 11:05:19 +0800

    作品集

---
+++ 微信小程序天气预报/pages/index/index.js
@@ -0,0 +1,89 @@
+﻿//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    weekday:'周五',
+    weekdays: ['周日','周一','周二','周三','周四','周五','周六'],
+    district:'天河区',
+    street:'林和路',
+    now:{},
+    forecast:{}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+   var date = new Date();//当前系统时间
+   date.setDate(date.getDate()+2);//将时间设成2天后
+   var i = date.getDay();//星期几
+   this.setData({//自动重新渲染页面
+     weekday:this.data.weekdays[i]
+   });
+
+   //调用getAddress函数
+   this.getAddress();
+  },
+
+  getAddress : function(){
+    var that = this;
+    wx.getLocation({
+      type:'wgs84',
+      success: function(res) {
+        var lat = res.latitude;//纬度
+        var lng = res.longitude;//经度
+
+        wx.request({
+          url: 'https://api.map.baidu.com/geocoder/v2/', //接口地址
+          data: {
+            location:lat+','+lng,
+            output: 'json',
+            ak:'QgDjg59KnbdsL14plwnoP5rUAGKyDYPe'
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            console.log(res.data);
+            that.setData({
+              district:res.data.result.addressComponent.district,
+              street: res.data.result.addressComponent.street
+            });
+
+            //调用getWeather函数获取天气数据
+            that.getWeather(res.data.result.addressComponent.district);
+          }
+        })
+      }
+    })
+  },
+
+
+
+  getWeather : function(city){
+    var that = this;
+    wx.request({
+      url: 'https://free-api.heweather.com/v5/weather',
+      /*和风天气预报接口 */
+      data: {
+        city: city,
+        key: '0db0ec37f8f84611ae94e49f17a6bd47'
+      },
+      header:{
+        'content-type': 'application/json'
+      },
+      success : function(res){
+        console.log(res);
+        that.setData({
+          now:res.data.HeWeather5["0"].now,
+          forecast:res.data.HeWeather5["0"].daily_forecast
+        })
+      }
+    })
+  }
+})
---
+++ 新春贺卡/js/script.js
@@ -0,0 +1,37 @@
+﻿window.onload = function(){
+	var disc = document.getElementById('disc');
+	var audio1 = document.getElementById('audio1');
+	var page1 = document.getElementById('page1');
+	var page2 = document.getElementById('page2');
+	var page3 = document.getElementById('page3');
+	var blessing = page3.getElementsByClassName('p3_blessing')[0];
+	
+	disc.onclick = function(){
+		//判断动画的状态
+		if(disc.style.animationPlayState=='paused'){
+			//原来是暂停的状态，点击后变成运行状态
+			disc.style.animationPlayState=='running';
+			audio1.play();
+		}else{
+			//原来是运行的状态，点击后变成暂停状态
+			disc.style.animationPlayState=='paused';
+			audio1.pause();
+		}
+	}
+	
+	page1.onclick = function(){
+		page1.style.display='none';
+		page2.style.display='block';
+		setTimeout(function(){
+			page3.style.display='block';
+			page2.className='page fadeOut';
+			page3.className='page fadeIn';
+			blessing.className = 'p3_blessing blessing_anim1';
+			
+			setTimeout(function(){
+				blessing.className = 'p3_blessing blessing_anim2';
+			},2000);
+		},5000);
+	}
+	
+}
